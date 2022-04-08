b6ae31a6507208707811222715d7d34360c6732e
A01746123jorge <a01746123@tec.mx>
2022-04-07 21:00:09 -0500

    Update login como primmera pag

--- app.py
+++ app.py
@@ -1,20 +1,42 @@
-from flask import Flask, render_template, request
+import re
+from flask import Flask, redirect, render_template, request, session, url_for
+import datetime
 
 # FlASK
 #############################################################
 app = Flask(__name__)
+app.permanent_session_lifetime = datetime.timedelta(days=365)
+app.secret_key = "super secret key"
 #############################################################
-
-@app.route("/login", methods=["GET", "POST"])
-def login():
-    if (request.method == "GET"):
-        return render_template("Login.html", data="email")
-    else:
-        email = None
-        email = request.form["email"]
-        password = request.form["password"]
-        return render_template("index.html", data=email)
 
 @app.route('/')
 def home():
+    email = None
+    if "email" in session:
+        email =session["email"]
+        return render_template('index.html',data = email)
+    else:
+        return render_template('Login.html',data = email)
+
+@app.route('/signup')
+def signup():
+    name=request.form["name"]
+    email=request.form["email"]
+    password=request.form["password"]
     return render_template('index.html')
+
+@app.route("/login", methods=["GET", "POST"])
+def login():
+    email = None
+    if "email" in session:
+        return render_template('index.html',data=session["email"])
+    else:
+        if (request.method == "GET"):
+            return render_template("Login.html", data ="email")
+        else:
+            email = None
+            email = request.form["email"]
+            password = request.form["password"]
+            session["email"] = email
+            return render_template("index.html", data=email)
+
