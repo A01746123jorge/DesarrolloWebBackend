13e93f61767d35e5c88ce631fa4053b5432fbcef
A01746123jorge <a01746123@tec.mx>
2022-04-07 20:13:55 -0500

    Se añadio login

--- app.py
+++ app.py
@@ -5,10 +5,6 @@ from flask import Flask, render_template, request
 app = Flask(__name__)
 #############################################################
 
-@app.route('/')
-def home():
-    return render_template('index.html')
-
 
 @app.route("/login", methods=["GET", "POST"])
 def login():
